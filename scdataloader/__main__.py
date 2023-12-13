b1e22f0ff0fefdce2d94144229186d34e63f5803
jkobject <jkobject@users.noreply.github.com>
2023-12-13 16:53:16 +0000

    ✅ Ready to clone and code.

---
+++ scdataloader/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for scdataloader."""
+
+from scdataloader.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from scdataloader.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "scdataloader"
