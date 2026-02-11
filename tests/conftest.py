b7cb471e60689619673f3d98e3f3bb91d7603dc6
jkobject <jkobject@gmail.com>
2026-02-11 10:06:17 +0100

    updating the conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,16 @@ import pytest
 
 
 def pytest_sessionstart():
-    ln.setup.init(storage="./testdb", name="test", modules="bionty")
+    ln.setup.init(
+        storage="./test-scdataloaderdb", name="test-scdataloader", modules="bionty"
+    )
     ln.settings.creation.artifact_silence_missing_run_warning = True
     ln.settings.track_run_inputs = False
 
 
 def pytest_sessionfinish(session):
-    shutil.rmtree("./testdb")
-    ln.setup.delete("test", force=True)
+    shutil.rmtree("./test-scdataloaderdb")
+    ln.setup.delete("test-scdataloader", force=True)
 
 
 ## each test runs on cwd to its temp dir
